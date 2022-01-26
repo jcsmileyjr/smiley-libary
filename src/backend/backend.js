de95098b06ba02715d7d452a6766abed8ef62067
Smiley <jcsmileyjr@gmail.com>
2022-01-26 15:37:07 -0600

    2nd attempt at a node backend

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -1,8 +1,19 @@
 let http = require("http");
+let data = require("./optimizeData");
 
 http
   .createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/html" });
-    response.end("Hello World!");
+    response.write(
+      JSON.stringify([
+        "How to grill burgers",
+        "How to grill chicken",
+        "How to grill sausages",
+        "How to fry fish",
+        "How to fry season fries",
+        "How to fry chicken",
+      ])
+    );
+    response.end();
   })
   .listen(8080);
---
+++ src/backend/optimizeData.js
@@ -0,0 +1,8 @@
+module.exports =[
+    "How to grill burgers",
+    "How to grill chicken",
+    "How to grill sausages",
+    "How to fry fish",
+    "How to fry season fries",
+    "How to fry chicken",
+  ];
